7ea4bc541ea446e929226aa92e6a327048192b10
luke <luke.zhang@megabank.com.tw>
2025-06-21 11:13:15 +0800

    fix DonationRoutes, add Swagger

--- src/main/java/com/irent/donation_backend/config/DonationRoutes.java
+++ src/main/java/com/irent/donation_backend/config/DonationRoutes.java
@@ -1,18 +1,30 @@
 package com.irent.donation_backend.config;
 
 import com.irent.donation_backend.api.DonationHandler;
+import com.irent.donation_backend.model.Customer;
 import com.irent.donation_backend.service.DonationService;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.fn.builders.operation.Builder;
+import org.springdoc.webflux.core.fn.SpringdocRouteBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.util.function.Consumer;
+
+import static org.springdoc.core.fn.builders.apiresponse.Builder.responseBuilder;
+import static org.springdoc.core.fn.builders.content.Builder.contentBuilder;
+import static org.springdoc.core.fn.builders.requestbody.Builder.requestBodyBuilder;
+import static org.springdoc.core.fn.builders.schema.Builder.schemaBuilder;
 
 @Configuration
 public class DonationRoutes {
 
     private static final String MAIN_REQUEST_MAPPING = "/api/donation";
+    private static final String GET_CUSTOMER_MAPPING = "/customer/{name}";
 
     @Bean
     public DonationHandler donationHandler(DonationService donationService) {
@@ -20,12 +32,33 @@ public class DonationRoutes {
     }
 
     @Bean
-    public RouterFunction<ServerResponse> donationRouterFunction(DonationHandler handler) {
-        return RouterFunctions.route()
-                .path(MAIN_REQUEST_MAPPING,
-                        builder -> builder
-                                .GET("/customer/{name}", handler::getCustomer)
-                )
-                .build();
+    public RouterFunction<ServerResponse> donationRouterFunctionSwagger(final DonationHandler handler) {
+        return getCustomerFunctionSwagger(handler);
+    }
+
+    protected RouterFunction<ServerResponse> getCustomerFunctionSwagger(final DonationHandler handler) {
+        final Consumer<Builder> result = ops -> {
+            ops.beanClass(DonationHandler.class)
+                    .beanMethod("getCustomer")
+                    .operationId("Get Customer Info")
+                    .summary("取得捐款目標機構資訊")
+                    .description("取得捐款目標機構資訊")
+                    .response(responseBuilder()
+                            .content(contentBuilder().mediaType(MediaType.APPLICATION_JSON_VALUE).schema(schemaBuilder().implementation(Customer.class)))
+                            .responseCode("200")
+                            .description("successful operation")
+                    );
+            commonProcess(ops);
+        };
+        return SpringdocRouteBuilder.route().path(MAIN_REQUEST_MAPPING,
+                builder -> builder.GET(GET_CUSTOMER_MAPPING, handler::getCustomer), result).build();
+    }
+
+    protected void commonProcess(final Builder builder) {
+        builder.response(responseBuilder().responseCode("400").description("Bad Request"))
+                .response(responseBuilder().responseCode("401").description("Unauthorized"))
+                .response(responseBuilder().responseCode("403").description("Forbidden"))
+                .response(responseBuilder().responseCode("404").description("Not found"))
+                .response(responseBuilder().responseCode("500").description("Internal Server Error"));
     }
 }
--- src/main/java/com/irent/donation_backend/model/Customer.java
+++ src/main/java/com/irent/donation_backend/model/Customer.java
@@ -1,5 +1,6 @@
 package com.irent.donation_backend.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,15 +12,21 @@ import java.math.BigDecimal;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "捐款目標資訊")
 public class Customer {
 
+    @Schema(description = "ID")
     private String id;
 
+    @Schema(description = "商店key")
     private String name;
 
+    @Schema(description = "商店value")
     private String value;
 
+    @Schema(description = "是否有預設金額")
     private Boolean isDefault;
 
+    @Schema(description = "預設金額")
     private BigDecimal amount;
 }
